c98600fe1b26c429f97b9845a6d578f3b3c12390
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2022-09-11 13:23:21 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,16 +1,15 @@
 let handler = async m => m.reply(`
-╭─「 Donasi 」
-│ • Tri [08989513496]
-│ • Dana • Gopay  [08989513496]
+╭─「 *GROUP BOT MULTI DEVICE* 」
+ • BEBAS MAIN BOT ATAU 
+ • TANYA TANYA TENTANG BOT 
+ • https://chat.whatsapp.com/GmXOLVjquxA7iYhTfak2Mt
 ╰────
 ╭─「 *NOTE* 」
 │ > Ingin donasi? Wa.me/628989513496
 │ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ _atau beli RDP/VPS agar bot bisa jalan_
 │ _24jam tanpa kendala_
 ╰────
-
-*｢ Made by ❤️ AINE ｣*
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
